4340bd0d9cfe2ad4abe97b6593c8f89f8f5d5fd2
Dmitry Solyannik <dmitry.solyannik@gmail.com>
2019-07-18 17:55:20 +0300

    Advanced search functionality, changed search by city

--- apis/advancedSearch/index.js
+++ apis/advancedSearch/index.js
@@ -1,69 +1,78 @@
-/*
-
- ----------------------------------------------------------------------------
- | fhir-mpi: Demonstration QEWD-Up FHIR-based MPI MicroService              |
- |                                                                          |
- | Copyright (c) 2019 M/Gateway Developments Ltd,                           |
- | Redhill, Surrey UK.                                                      |
- | All rights reserved.                                                     |
- |                                                                          |
- | http://www.mgateway.com                                                  |
- | Email: rtweed@mgateway.com                                               |
- |                                                                          |
- |                                                                          |
- | Licensed under the Apache License, Version 2.0 (the "License");          |
- | you may not use this file except in compliance with the License.         |
- | You may obtain a copy of the License at                                  |
- |                                                                          |
- |     http://www.apache.org/licenses/LICENSE-2.0                           |
- |                                                                          |
- | Unless required by applicable law or agreed to in writing, software      |
- | distributed under the License is distributed on an "AS IS" BASIS,        |
- | WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. |
- | See the License for the specific language governing permissions and      |
- |  limitations under the License.                                          |
- ----------------------------------------------------------------------------
-
-  12 March 2019
-
-*/
 
 module.exports = function(args, finished) {
-  let query = args.req.query;
-  if (query.value === '') {
-    return finished({error: 'Search value was not defined'});
-  }
-  
-  let params = {};
-  let patientIndex = this.db.use('PatientIndex', query.type, query.value);
-  
-  if (query.type === 'by_age') {
-    const date = new Date();
-    const from = date.getFullYear() - query.from +1;
-    const to = date.getFullYear() - query.to;
-    
-    params = {
-      range: {
-        from: `${to}-${('0' + (date.getMonth()+1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`,
-        to: `${from}-${('0' + (date.getMonth()+1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`
-      }
-    };
-    patientIndex = this.db.use('PatientIndex', 'by_birthdate');
-  }
-  
+  const body = args.req.body;
   const fhir = {
     resourceType: 'Bundle',
     entry: []
   };
   
-  const patientDoc = this.db.use('Patient', 'by_id');
+  const filterByField = function (patient) {
+    if (body.gender && body.gender === patient.gender) {
+      fhir.entry.push({
+        resource: patient
+      });
+    } else if (body.firstName && body.lastName && body.firstName.toLowerCase() === patient.name[0].given[0].toLowerCase() && body.lastName.toLowerCase() === patient.name[0].family.toLowerCase()) {
+      fhir.entry.push({
+        resource: patient
+      });
+    } else if (!body.firstName && body.lastName && body.lastName.toLowerCase() === patient.name[0].family.toLowerCase()) {
+      fhir.entry.push({
+        resource: patient
+      });
+    } else if (!body.lastName && body.firstName && body.firstName.toLowerCase() === patient.name[0].given[0].toLowerCase()) {
+      fhir.entry.push({
+        resource: patient
+      });
+    }
+    if (!body.firstName && !body.lastName && !body.gender) {
+      fhir.entry.push({
+        resource: patient
+      });
+    }
+  };
   
-  patientIndex.forEachChild(params, function(id, node) {
-    const identifier = query.type === 'by_age' &&  node.firstChild.name ? node.firstChild.name : id;
-    const patient = patientDoc.$(identifier).getDocument(true);
+  let patientIndex = body.nhsNumber ? this.db.use('PatientIndex', 'by_identifier', body.nhsNumber) : this.db.use('PatientIndex', 'by_birthdate');
+  
+  const date = new Date();
+  const from = date.getFullYear() - body.from +1;
+  const to = date.getFullYear() - body.to;
+  
+  let params = {
+    range: {
+      from: `${to}-${('0' + (date.getMonth()+1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`,
+      to: `${from}-${('0' + (date.getMonth()+1)).slice(-2)}-${('0' + date.getDate()).slice(-2)}`
+    }
+  };
+  
+  if (body.nhsNumber) {
+    if (!patientIndex.exists) {
+      return finished({
+        error: 'The specified Patient Id does not exist',
+        status: {
+          code: 404
+        }
+      });
+    }
+    const patient = this.db.use('Patient', 'by_id', patientIndex.firstChild.name).getDocument(true);
     fhir.entry.push({
       resource: patient
     });
+    finished(fhir);
+  }
+  
+  const patientDoc = this.db.use('Patient', 'by_id');
+  if (body.birthDate) {
+    
+    patientIndex = this.db.use('PatientIndex', 'by_birthdate', body.birthDate);
+    params = {};
+  }
+  patientIndex.forEachChild(params, function(id, node) {
+    const ptId = body.birthDate ? id : node.firstChild.name;
+    
+    if (ptId) {
+      const patient = patientDoc.$(ptId).getDocument(true);
+      filterByField(patient);
+     }
   });
   
   finished(fhir);
